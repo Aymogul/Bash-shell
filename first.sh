3cd19108e95e5047aac3956fbfc4593876de9968
Aymogul <aymogul22@gmail.com>
2024-08-28 15:47:21 -0700

    first.sh

---
+++ first.sh
@@ -0,0 +1,19 @@
+
+
+#!/bin/bash
+
+echo "Hello my lovely script"
+# sudo apt install git -y
+# this is used to install git unto your server
+
+# sudo apt install docker.io
+# this is used t install docker unto your server
+
+
+x=5
+y=7
+
+echo $((x + y))
+
+echo `pwd`
+
