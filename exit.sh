4f4319f9260db0e4fd70502cc61d05a06f5bf49e
Aymogul <aymogul22@gmail.com>
2024-08-29 05:33:02 -0700

    exit.sh

---
+++ exit.sh
@@ -0,0 +1,6 @@
+# this is to check for the exit stauts of the last executed command
+
+echo "$?"
+
+# 0 if the last command  executed was successful 
+# 1 if the last executed command was unsuccessful
