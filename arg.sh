19b51f697c7e764edc53d944e87f878133297813
Aymogul <aymogul22@gmail.com>
2024-08-29 05:28:46 -0700

    args

---
+++ arg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# script that demos, the command line arguments
+
+echo "Total number of command line argument are $#"
+
+echo "$0 is the script name"
+
+echo "$1 is the forst argument"
+
+echo "$2 is the second argument"
+
+echo "All of them are :- $* or $@"
+
+
