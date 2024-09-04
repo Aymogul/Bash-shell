5a6a591874020ddbf907aaeb3ca2779b50e47b0d
Aymogul <aymogul22@gmail.com>
2024-09-04 05:58:17 -0700

    mod array

--- array.sh
+++ array.sh
@@ -1,5 +1,8 @@
-NAME01="Zara" 
-NAME02="Qadir" 
-NAME03="Mahnaz" 
-NAME04="Ayan" 
-NAME05="Daisy"
+#!/bin/sh 
+NAME[0]="Zara" 
+NAME[1]="Qadir" 
+NAME[2]="Mahnaz" 
+NAME[3]="Ayan" 
+NAME[4]="Daisy" 
+echo "First Index: ${NAME[0]}" 
+echo "Second Index: ${NAME[1]}"
